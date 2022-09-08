32664d2f999bcf79e57cefa92f8f064e3314a6d4
Joel Ical <2005113038@ub.edu.bz>
2022-09-08 16:17:12 -0600

    created a school struct that holds all the information of a school

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -5,6 +5,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"appletree.joelical.net/internal/data"
 )
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
@@ -19,6 +22,22 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 		http.NotFound(w, r)
 		return
 	}
-	//Display the shcool id
-	fmt.Fprintf(w, "show the details for the school %d\n", id)
+
+	//Create a new instance of the school struct containing the ID we extracted from our URL and some sample data
+	school := data.School{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Name:      "Apple Tree",
+		Level:     "High School",
+		Contact:   "Anna Smith",
+		Phone:     "601-4411",
+		Address:   "4 Blue Hole Street",
+		Mode:      []string{"blended", "online"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, school, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and count not process your request", http.StatusInternalServerError)
+	}
 }
